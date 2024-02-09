3b874b98829aa0d41d59710ffc2b6c71f474fc6d
antonymundanmany <antonymundanmany@gmail.com>
2024-02-09 11:25:08 +0530

    updated

---
+++ models/blogModel.js
@@ -0,0 +1,17 @@
+
+const mongoose = require("mongoose")
+
+const blogSchema = new mongoose.Schema(
+
+{
+    name:String,
+    age:String,
+    mobilenumber:String,
+    address:String,
+    pincode:String,
+    emailid:String,
+    password:String
+}
+
+)
+module.exports=mongoose.model("blogs",blogSchema)
